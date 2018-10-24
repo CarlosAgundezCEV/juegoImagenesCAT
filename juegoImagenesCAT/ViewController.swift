05e284c347b0e410f69529347f8e362a204444dc
CarlosAgundezCEV <carlos_agundez_apps1ma1718@cev.com>
2018-10-24 11:50:01 +0200

    Initial Commit

---
+++ juegoImagenesCAT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juegoImagenesCAT
+//
+//  Created by wizO on 24/10/2018.
+//  Copyright © 2018 Carlos Agundez Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
